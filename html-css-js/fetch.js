02f911a5c1429bd2f97f60a16e4ac27fd70a674c
DUNGLA <dungla2011@gmail.com>
2023-11-22 17:15:59 +0700

    fetch post api

--- html-css-js/fetch.js
+++ html-css-js/fetch.js
@@ -1,4 +1,4 @@
-var url = 'https://jsonplaceholder.typicode.com/posts';
+var url = 'https://jsonplaceholder.typicode.com/todos';
 fetch(url)
     .then(function(response) {
         return response.json();
@@ -7,8 +7,8 @@ fetch(url)
         console.log(posts);
         let htmls = posts.map(function(post) {
             return `<li>
-                <h2>${post.title}</h2>
-                <p>${post.body}</p>
+                <h2>${post.id}</h2>
+                <p>${post.title}</p>
             </li>`;
         });
         let html = htmls.join('');
